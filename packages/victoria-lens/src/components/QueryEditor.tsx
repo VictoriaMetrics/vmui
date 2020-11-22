03cdd185d6f0e4b53f78b5eff441fa040f6234f6
Ihor Tsvietkov <tsvietko@progress.com>
2020-11-22 18:14:49 +0200

    autocomplete setting react on changes in server settings

--- packages/victoria-lens/src/components/QueryEditor.tsx
+++ packages/victoria-lens/src/components/QueryEditor.tsx
@@ -8,41 +8,47 @@ import { basicSetup } from '@codemirror/next/basic-setup';
 export interface QueryEditorProps {
   setQuery: (query: string) => void;
   query: string;
+  server: string;
 }
 
-// TODO: use value from input
-const promQL = new PromQLExtension().setComplete({url: 'http://127.0.0.1:8428'})
-
-const QueryEditor: FC<QueryEditorProps> = ({query, setQuery}) => {
+const QueryEditor: FC<QueryEditorProps> = ({query, setQuery, server}) => {
 
   const ref = useRef<HTMLDivElement>(null)
 
   const [editorView, setEditorView] = useState<EditorView>();
 
+  // init editor view on load
   useEffect(() => {
     if (ref.current) {
-
-      const listenerExtension = EditorView.updateListener.of(editorUpdate => {
-        if (editorUpdate.docChanged) {
-          setQuery(
-              editorUpdate.state.doc.toJSON().map(el => el.trim()).join("")
-          );
-        }
-
-      })
-
       setEditorView(new EditorView(
         {
-          state: EditorState.create({
-            doc: query,
-            extensions: [basicSetup, keymap(defaultKeymap), listenerExtension, promQL.asExtension()]
-          }),
           parent: ref.current
         })
       )
-      return () => editorView?.destroy();
     }
+    return () => editorView?.destroy();
   }, [])
+
+  // update state on change of autocomplete server
+  useEffect(() => {
+
+    const promQL = new PromQLExtension().setComplete({url: server});
+
+    const listenerExtension = EditorView.updateListener.of(editorUpdate => {
+      if (editorUpdate.docChanged) {
+        setQuery(
+            editorUpdate.state.doc.toJSON().map(el => el.trim()).join("")
+        );
+      }
+
+    })
+
+    editorView?.setState(EditorState.create({
+      doc: query,
+      extensions: [basicSetup, keymap(defaultKeymap), listenerExtension, promQL.asExtension()]
+    }))
+
+  }, [server, editorView])
 
   return (
       <>
